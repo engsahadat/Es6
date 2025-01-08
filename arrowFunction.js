07d66a46b85a60451ebbe243ee5b94444acd69c7
engsahadat <eng.md.sahadathossain@gmail.com>
2025-01-08 23:46:24 +0600

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,30 @@
+ 
+// function learn(){
+//     return "Hello";
+// }
+// const learn = function(){
+//     return "Hello";
+// }
+
+const learn = () =>{
+    return "Hello";
+}
+// console.log(learn());
+
+let myFunction = (a,b)=> a*b;
+// console.log(myFunction(20,10));
+hello = function() {
+    return "Hello World!";
+  }
+//   console.log(hello());
+
+// hello = () => "Hello World";
+// console.log(hello());
+hello = () => {
+    return "Hello World!";
+}
+// console.log(hello());
+
+
+
+  
